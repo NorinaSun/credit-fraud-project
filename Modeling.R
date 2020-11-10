0cb4cb29be6e7a79da69127677c43195bc98ac4c
Norina Sun <norina_sun@yahoo.ca>
2020-11-10 08:52:45 -0500

    updated data file to csv

--- Modeling.R
+++ Modeling.R
@@ -1,50 +1,92 @@
 library(class)
 library(data.table)
+knitr::opts_chunk$set(echo = TRUE)
+library(tidyverse,verbose=FALSE)
+library(Rlab)  
+library(caret)
+library(adabag)
+library(randomForest,verbose=FALSE)
 
-#generate some artificial data
-sample = data.frame(replicate(10,sample(0:1,1000,rep=TRUE)))
+#load the data
+sim100M = read.csv("/Users/NorinaSun/Downloads/MATH60603/GroupProject/CreditFraudProject/sim100M.csv")
 
-#define function that receives observation and predicts using a fake model
-predict = function(data){
-  #grab the observations
-  obs_predict = data[nrow(data),]
-  obs_train = data[1:nrow(data)-1,]
+#predict and train functions
+train_md = function(data,md){
   
-  # Start the clock!
-  ptm <- proc.time()
-  
-  #fit and predict
-  model <- knn(obs_train[,-10],obs_predict[,-10],cl=obs_train$X10,k=10)
+  if(md == "reg"){
+    
+    # Start the clock!
+    ptm <- proc.time()
+    
+    trained_model = glm(as.factor(unlist(FraudResults))~.,data=data,family="binomial")
+  }
+  else if(md == "rf"){
+    
+    # Start the clock!
+    ptm <- proc.time()
+    
+    trained_model = randomForest(as.factor(FraudResults)~CreditLimit+ShoppingFreq+TransactionAmount+HowMuch+OnlineTransaction,data=data)
+  }
+  else if(md == "boosting"){
+    data$FraudResults = as.factor(unlist(data$FraudResults))
+
+    # Start the clock!
+    ptm <- proc.time()
+    
+    trained_model = boosting(FraudResults~.,data=data)
+  }
+  else{
+    print("invalid model selection passed")
+  }
   
   # Stop the clock
   runtime = proc.time() - ptm
   elapsed_time = runtime[3]
-
-  return(elapsed_time)
+  
+  #return the model and the elapsed time
+  return(list("trained_model"= trained_model, "time"= elapsed_time))
 }
 
-#define function that sends the obvs
-stream = function(data,min_n){
-  summed_time = 0
-  time_list = numeric(0)
+predict_md = function(data,model){
+  
+  # Start the clock!
+  ptm <- proc.time()
+  
+  #generate the prediction
+  prediction = predict(model,newdata = data)
+  
+  # Stop the clock
+  runtime = proc.time() - ptm
+  elapsed_time = runtime[3]
+  
+  #return the prediction value and the elapsed time
+  return(list("prediction"= prediction, "time" = elapsed_time ))
+}
+
+#streaming simulation
+streaming = function(data,min_n,md){
+  
+  train_list_time = numeric(0)
+  predict_list_time = numeric(0)
+  
   for(i in min_n:nrow(data)){
-    #getting prediction time
-    elapsed_time = predict(data[0:i,])
-    summed_time = summed_time + elapsed_time
-    #adding the time to a list
-    average_time = summed_time/nrow(data)
-    time_list[i] = average_time
+    sprintf("now on data size", i)
+
+    #training the model
+    train_results = train_md(data[0:i,],md)
+    #getting what it returns
+    trained_model = train_results["trained_model"]
+    train_list_time[i] = train_results["time"]
+ 
+    #predicting with new observation
+    predict_results = predict_md(data[i:nrow(data),], trained_model)
+    #getting what it returns
+    predictions = predict_results["prediction"]
+    predict_list_time[i] = as.numeric(predict_results["time"]) / (nrow(data) - i)
   }
-  return(time_list)
+  return(list("train_time" = train_list_time, "predict_time" = predict_list_time))
 }
 
-#specify min number of observations to start with. Make sure its > k
-min_n = 50
-
-#get the results
-results = data.frame(stream(sample,min_n))
-
-#plot the results
-plot(seq(min_n,nrow(results)),results[min_n:nrow(results),1],
-     xlab = "Number of Observations", ylab = "Time for Fit and Predictions (in seconds)",
-     main = "Time Versus Dataset Size")
+results = streaming(sim100M, 1000, "rf")
+train_time = results["train_time"]
+predict_time = results["predict_time"]
---
+++ ToyExample.R
@@ -0,0 +1,50 @@
+library(class)
+library(data.table)
+
+#generate some artificial data
+sample = data.frame(replicate(10,sample(0:1,1000,rep=TRUE)))
+
+#define function that receives observation and predicts using a fake model
+predict = function(data){
+  #grab the observations
+  obs_predict = data[nrow(data),]
+  obs_train = data[1:nrow(data)-1,]
+  
+  # Start the clock!
+  ptm <- proc.time()
+  
+  #fit and predict
+  model <- knn(obs_train[,-10],obs_predict[,-10],cl=obs_train$X10,k=10)
+  
+  # Stop the clock
+  runtime = proc.time() - ptm
+  elapsed_time = runtime[3]
+  
+  return(elapsed_time)
+}
+
+#define function that sends the obvs
+stream = function(data,min_n){
+  summed_time = 0
+  time_list = numeric(0)
+  for(i in min_n:nrow(data)){
+    #getting prediction time
+    elapsed_time = predict(data[0:i,])
+    summed_time = summed_time + elapsed_time
+    #adding the time to a list
+    average_time = summed_time/nrow(data)
+    time_list[i] = average_time
+  }
+  return(time_list)
+}
+
+#specify min number of observations to start with. Make sure its > k
+min_n = 50
+
+#get the results
+results = data.frame(stream(sample,min_n))
+
+#plot the results
+plot(seq(min_n,nrow(results)),results[min_n:nrow(results),1],
+     xlab = "Number of Observations", ylab = "Time for Fit and Predictions (in seconds)",
+     main = "Time Versus Dataset Size")
